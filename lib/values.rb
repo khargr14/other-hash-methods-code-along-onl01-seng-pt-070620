c3efcd66bd9d2e549519c6d3b05b84ae78e0394a
Keesha Hargrove <kharg14@gmail.com>
2020-07-17 00:03:29 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,5 +11,5 @@ require 'pry'
 def get_the_values(groceries)
   #code your solution here!
   groceries.values 
-  groceries.values.flatten options_from_collection_for_select
+  groceries.values.flatten 
 end
